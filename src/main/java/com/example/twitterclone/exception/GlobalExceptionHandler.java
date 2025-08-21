5a5f99db6705f83de5cafae7408c4e89e86aeb5c
sena <gulozseven@gmail.com>
2025-08-21 14:35:10 +0300

    errorResponse

--- src/main/java/com/example/twitterclone/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/twitterclone/exception/GlobalExceptionHandler.java
@@ -1,21 +1,41 @@
 package com.example.twitterclone.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice //to make it intervene when an error occurs.
 public class GlobalExceptionHandler {
 
+    //method overloading
 
     //application'da yazarken bizim throw ettiğimiz hataları  handle edecek. --> yukarıda olması çalışması içim mühim.
+    //bu application'ın herhangi bir yerinde hata çıkarsa buraya düşecek
     @ExceptionHandler
     public ResponseEntity<TwitterErrorResponse> handleException(TwitterException twitterException) {
-        return null;
+        TwitterErrorResponse response = new TwitterErrorResponse(
+                twitterException.getMessage(),
+                twitterException.getHttpStatus().value(),
+                System.currentTimeMillis(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(response, twitterException.getHttpStatus());
     }
 
+
+    //kalan tüm hatalar da buraya düşecek.
+    //http status gelmeyecek, biz yazacağız: bizim düşünemediğimiz durumlarda hata çıkarsa: 500
     @ExceptionHandler
     public ResponseEntity<TwitterErrorResponse> handleException(Exception exception) {
-        return null;
+        TwitterErrorResponse response = new TwitterErrorResponse(
+                exception.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                System.currentTimeMillis(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/example/twitterclone/exception/TwitterErrorResponse.java
+++ src/main/java/com/example/twitterclone/exception/TwitterErrorResponse.java
@@ -1,4 +1,19 @@
 package com.example.twitterclone.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class TwitterErrorResponse {
+
+    private String message;
+    private int status;
+    private long timestamp;
+    private LocalDateTime localDateTime;
 }
