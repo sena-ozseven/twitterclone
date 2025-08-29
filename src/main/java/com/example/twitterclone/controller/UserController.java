f201586df35341f0f42febe4b9fe26eaad7cc90d
sena <gulozseven@gmail.com>
2025-08-29 15:25:49 +0300

    Update UserController.java

--- src/main/java/com/example/twitterclone/controller/UserController.java
+++ src/main/java/com/example/twitterclone/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -26,7 +28,7 @@ public class UserController {
         return ResponseEntity.ok(userProfile);
     }
 
-    //HTTP PATCH --> update 
+    //HTTP PATCH --> update
     @PatchMapping("/{id}")
     public ResponseEntity<UserResponseDto> updateUserProfile(@PathVariable Long id,
                                                              @Valid @RequestBody UserUpdateRequestDto updateRequest) {
